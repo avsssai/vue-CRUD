d382f613b749c88583f65d03ecfa25acd224dd6d
avsssai <34810623+avsssai@users.noreply.github.com>
2019-03-28 14:16:37 +0530

    CRUD operations working, must fix some frontend issues

--- frontend/src/components/CreateTodo.vue
+++ frontend/src/components/CreateTodo.vue
@@ -1,53 +1,47 @@
 <template>
-
-<div class="main">
-  <div class="container" id='todo-list'>
-    <div class="row">
-      <div class="col-md-6 mx-auto">
-          <h1 class='text-center'>TODO LIST</h1>
-          <form v-on:submit='createTodo()'>
-            <input type="text" class='form-control' placeholder='Enter the task..' v-model='taskname'>
-            <button class='btn-success btn-block mt-3'  type='submit'>Add Task</button>
+  <div class="main">
+    <div class="container" id="todo-list">
+      <div class="row">
+        <div class="col-md-6 mx-auto">
+          <h1 class="text-center">TODO LIST</h1>
+          <form v-on:submit="createTodo()">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Enter the task.."
+              v-model="taskname"
+            >
+            <button class="btn-success btn-block mt-3" type="submit">Add Task</button>
             <!-- <button class='btn-primary btn-block mt-3' v-if="this.isEditing==true" @click="updateTask()">Update Task</button> -->
-        </form>
-        <table class='table'>
-          
-          <tr v-for='todo in todos' v-bind:key='todo.id' v-bind:title='todo.title' >
-            <td class='text-left'  >
-                <div v-show="isEditing===false" @dblclick="showEditingForm()" v-on:blur="isEditing=false" >{{todo.title}}
+          </form>
+          <table class="table">
+            <tr v-for="todo in todos" v-bind:key="todo.id" v-bind:title="todo.title">
+              <td class="text-left">
+                <div
+                  v-show="isEditing===false"
+                  @dblclick="showEditingForm()"
+                  v-on:blur="isEditing=false"
+                >{{todo.title}}</div>
+                <div v-show="isEditing===true">
+                  <input type="text" v-bind:id="todo.id" v-model="todo.title">
                 </div>
-                <div v-show="isEditing===true" >
-                    <input type="text" v-bind:id='todo.id' v-model="todo.title">
-                </div>
-
-            </td>
-            <td class='text-right' >
+              </td>
+              <td class="text-right">
                 <div v-if="isEditing===false">
-                                  <button @click='showEditingForm(todo.title)' class="btn btn-primary mr-3">
-                  Edit
-              </button>
-              <button @click='deleteTodo(todo._id)' class="btn btn-secondary">
-                  Delete
-              </button>
-
+                  <button @click="showEditingForm(todo.title)" class="btn btn-primary mr-3">Edit</button>
+                  <button @click="deleteTodo(todo._id)" class="btn btn-secondary">Delete</button>
                 </div>
 
                 <div v-if="isEditing===true">
-                    <button class="btn btn-primary" @click="updateTodo(todo._id)"> Update</button>
+                  <button class="btn btn-primary" @click="updateTodo(todo)">Update</button>
                 </div>
-
-            </td>
-
-            
-          </tr>
-          
-        </table>
-        
+              </td>
+            </tr>
+          </table>
+        </div>
       </div>
     </div>
-    
   </div>
-</div>
 </template>
 
 
@@ -56,57 +50,57 @@
 import axios from "axios";
 
 export default {
-    data(){
-        return{
-            todos:[],
-            taskname:'',
-            isEditing:false
-        }
+  data() {
+    return {
+      todos: [],
+      taskname: "",
+      isEditing: false
+    };
+  },
+  mounted() {
+    this.getTodos();
+  },
+  methods: {
+    getTodos() {
+      axios
+        .get("http://localhost:4000/todos")
+        .then(result => {
+          this.todos = result.data;
+        })
+        .catch(err => {
+          return err;
+        });
     },
-    mounted(){
+    createTodo() {
+      axios
+        .post("http://localhost:4000/todos/create", { title: this.taskname })
+        .then(() => {
+          if (this.taskname == "") {
+            alert("You cannot leave the todo empty.");
+            this.getTodos();
+          }
+          this.taskname = "";
+          this.getTodos();
+        })
+        .catch(err => {
+          return err;
+        });
+    },
+    deleteTodo(id) {
+      axios.delete(`http://localhost:4000/todos/${id}`).then(() => {
         this.getTodos();
+        this.taskname = "";
+      });
     },
-    methods:{
-        getTodos(){
-            axios.get('http://localhost:4000/todos')
-            .then(result=>{
-                this.todos = result.data;
-            })
-            .catch(err=>{
-                return (err);
-            })
-        },
-        createTodo(){
-            axios.post('http://localhost:4000/todos/create',{title:this.taskname})
-                .then(()=>{
-                    if(this.taskname==''){
-                        alert('You cannot leave the todo empty.');
-                        this.getTodos();
-                    }
-                    this.taskname ='';
-                    this.getTodos();
-                })
-                .catch(err=>{
-                return (err);
-                });
-        },
-        deleteTodo(id){
-            axios.delete(`http://localhost:4000/todos/${id}`)
-            .then(()=>{
-                this.getTodos();
-                this.taskname='';
-            })
-        },
-        showEditingForm(){
-            this.isEditing=true;
-        },
-        updateTodo(id){
-            axios.put(`http://localhost:4000/todos/${id}`)
-                .then(()=>{
-                    this.getTodos();
-                    this.isEditing=false;
-                });
-        }
+    showEditingForm() {
+      this.isEditing = true;
+    },
+    updateTodo(todo) {
+      axios.put(`http://localhost:4000/todos/${todo._id}`,{title:todo.title}).then(() => {
+        this.getTodos();
+        this.isEditing = false;
+      });
     }
-}
+  }
+};
 </script>
